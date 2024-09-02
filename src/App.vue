92a67baa192a45a3545edbb9a0cd2dc0dce188bc
Artyom Rebrik <arebrikdev@gmail.com>
2024-09-03 01:34:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,231 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+
+const tabs = [
+  { name: '⭐ П Мир 2004783645268', current: true },
+  { name: 'В Visa 64549002745', current: false },
+  { name: 'В МИР 4657374960', current: false },
+]
+
+const transactions = [
+  {
+    id: 1,
+    date: "07.06.2023",
+    time: "12:15",
+    type: "Траты",
+    sum: "-304.00",
+    place: "BURGER KING 0794",
+    mcc: "5311",
+    merchantID: "781000045916"
+  },
+  {
+    id: 2,
+    date: "07.06.2023",
+    time: "12:15",
+    type: "Пополнение",
+    sum: "1 584.00",
+    place: "BANKRNKOFOODCRD",
+    mcc: "5411",
+    merchantID: "BANKFOODCRD"
+  },
+  {
+    id: 3,
+    date: "07.06.2023",
+    time: "12:15",
+    type: "Списание за СМС",
+    sum: "-99.00",
+    place: "BANKRNKOFOODCRD",
+    mcc: "5732",
+    merchantID: "BANKFOODCRD"
+  },
+  {
+    id: 4,
+    date: "07.06.2023",
+    time: "12:15",
+    type: "Траты",
+    sum: "-290.00",
+    place: "EVO_KAFE",
+    mcc: "5814",
+    merchantID: "071000047543"
+  },
+]
+</script>
+
+<template>
+  <Header />
+
+  <main>
+    <div class="px-8 mt-5 max-w-[1470px] mx-auto">
+      <!-- User info -->
+
+      <div class="flex items-start justify-between flex-wrap">
+        <div>
+          <h1 class="text-2xl tracking-[-0.01em] mr-4 md:mr-0">Иванов Иван Константинович</h1>
+          <div class="mt-2 text-base font-medium text-[#757575] flex itemc-center gap-x-4 flex-wrap">
+            <div class="flex items-center">
+              <span class="mr-1 font-bold">ID</span>
+              <span>566534</span>
+            </div>
+            <div class="flex items-center">
+              <span class="mr-1 font-bold">🗒</span>
+              <span>Омск</span>
+            </div>
+            <div class="flex items-center">
+              <span class="mr-1 font-bold">📧</span>
+              <span>ivanovik@docke.ru</span>
+            </div>
+          </div>
+        </div>
+        <div class="mt-4 flex items-center gap-x-4 sm:gap-x-6 md:mt-0">
+          <div class="w-[80px] h-[43px] bg-[#AFF4C6] flex items-center justify-center border-4 rounded border-[#8CC39E] sm:w-[119px] sm:h-[43px]">Работает</div>
+          <button class="w-[160px] h-[43px] bg-[#FFA629] flex items-center justify-center rounded sm:w-[176px] sm:h-[43px]">✏️ Редактировать</button>
+        </div>
+      </div>
+
+      <!-- Tabs -->
+      <div>
+        <button class="mt-2 text-lg text-[#B3B3B3] sm:text-xs sm:mt-0">&lt; назад</button>
+
+        <div class="sm:hidden">
+          <select class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-black ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6">
+            <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
+          </select>
+        </div>
+
+        <nav class="hidden mt-1 sm:flex" aria-label="Tabs">
+          <a v-for="tab in tabs" :key="tab.name" href="#" :class="[tab.current ? 'border-black text-black font-bold' : 'border-b-[3px] border-[#D4D4D4] text-[#B3B3B3]', 'flex border-b-[3px] py-[10px] px-[25px] text-base max-w-[250px]']">{{ tab.name }}</a>
+        </nav>
+
+        <span class="mt-8 block text-base font-bold text-black">EAN 2004783645268</span>
+        <span class="mt-1 block text-base text-black">Пластиковая Мир - 05/2024</span>
+
+        <div class="mt-2 w-[112px] h-[32px] bg-[#FFCD29] flex items-center justify-center border-4 rounded border-[#CCA421]">Основная</div>
+
+        <div class="mt-8 overflow-hidden">
+          <div class="py-4 grid grid-cols-1 max-w-[954px] border-t-[3px] border-[#EEEEEE] sm:grid-cols-[231px_1fr]">
+            <div class="flex items-center">
+              <span class="text-base text-black block mr-1">Баланс счета</span>
+              <div class="group relative flex items-center">
+                <button>
+                  <img class="w-4 h-4 cursor-pointer hover:" src="./assets/img/info.png" alt="Info image">
+                </button>
+                <span class="group-hover:opacity-100 transition-opacity bg-[#E6E6E6] p-1 text-[11px] text-[#1E1E1E] rounded-md absolute  opacity-0 m-4 mx-auto min-w-[132px] left-5 top-1">Бвланс, оставшийся на счету</span>
+              </div>
+            </div>
+            <div class=" flex items-center">
+              <p class="text-base text-black mr-1 flex items-center">1 003 руб. <span class="text-sm text-[#9F9F9F] block ml-2">(обновлено 07.09.2023 13:56)</span></p>
+              <div class="group relative flex items-center">
+                <button>
+                  <img class="w-4 h-4 cursor-pointer hover:" src="./assets/img/info.png" alt="Info image">
+                </button>
+                <span class="group-hover:opacity-100 transition-opacity bg-[#E6E6E6] p-1 text-[11px] text-[#1E1E1E] rounded-md absolute  opacity-0 m-4 mx-auto min-w-[132px] left-5 top-1">Данные обновляются
+                  один раз в сутки,
+                  в будние дни</span>
+              </div>
+            </div>
+          </div>
+          <div class="py-4 grid grid-cols-1  max-w-[954px] border-t-[3px] border-[#EEEEEE] sm:grid-cols-[231px_1fr]">
+            <div class="flex items-center">
+              <span class="text-base text-black block mr-1">Баланс карты</span>
+              <div class="group relative flex items-center">
+                <button>
+                  <img class="w-4 h-4 cursor-pointer hover:" src="./assets/img/info.png" alt="Info image">
+                </button>
+                <span class="group-hover:opacity-100 transition-opacity bg-[#E6E6E6] p-1 text-[11px] text-[#1E1E1E] rounded-md absolute  opacity-0 m-4 mx-auto min-w-[132px] left-5 top-1">Бвланс, оставшийся на карте</span>
+              </div>
+            </div>
+            <div class="flex items-center">
+              <span class="text-base text-black mr-1 flex items-center">1 815 руб.</span>
+            </div>
+          </div>
+          <div class="py-4 grid-cols-1 grid max-w-[954px] border-t-[3px] border-[#EEEEEE] sm:grid-cols-[231px_1fr]">
+            <div class="flex items-center">
+              <span class="text-base text-black block mr-1">Можно пополнить на</span>
+            </div>
+            <div class="flex items-center">
+              <p class="text-base text-black mr-1 flex items-center">13 997 руб.</p>
+            </div>
+          </div>
+          <div class="py-4 grid grid-cols-1 max-w-[954px] border-t-[3px] border-b-[3px] border-[#EEEEEE] sm:grid-cols-[231px_1fr]">
+            <div class="flex items-center">
+              <span class="text-base text-black block mr-1">Лимит</span>
+            </div>
+            <div class="flex items-center">
+              <p class="text-base text-black mr-1 flex items-center">8 000 руб./мес.; пн, вт, ср, чт, пт, сб, вс; 08:00-17:00</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Table -->
+      <div class="flex flex-col mt-10 mb-8">
+        <h2 class="text-2xl tracking-[-0.01em]">История транзакций</h2>
+
+        <button class="mt-2 w-[144px] h-[33px] bg-[#FFA629] flex items-center justify-center rounded lg:self-end lg:mt-0">Скачать .xlsx</button>
+
+        <div class="mt-3">
+          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div class="overflow-hidden shadow sm:ring-1 sm:ring-black sm:ring-opacity-[0.2] sm:rounded-lg">
+                <table class="min-w-full border-spacing-0 sm:rounded-lg">
+                  <thead class="bg-[#F4F4F4]">
+                    <tr>
+                      <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-base font-normal border border-black border-opacity-20 sm:pl-6">N</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Дата</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Время</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Тип транзакции</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Сумма, руб.</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Место</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">MCC</th>
+                      <th scope="col" class="px-3 py-3.5 text-left font-normal text-base border border-black border-opacity-20">Merchant ID</th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    <tr v-for="(transaction, index) in transactions" :key="index">
+                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm border border-black border-opacity-20 sm:pl-6">{{ index + 1 }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20">{{ transaction.date }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20">{{ transaction.time }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20">{{ transaction.type }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20 text-right">{{ transaction.sum }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20 text-right">{{ transaction.place }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20 text-right">{{ transaction.mcc }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-base border border-black border-opacity-20 text-right">{{ transaction.merchantID }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-5 flex flex-col gap-y-4 items-center justify-between sm:flex-row lg:mt-3">
+          <div class="flex items-center gap-x-2 text-[#757575] text-sm">
+            <input class="w-10 pl-1 bg-[#d5d5d5] rounded" type="number" min="1" max="15" value="10" />
+            <span>Записи с 1 по 10 из 36 записей</span>
+          </div>
+
+          <div class="flex items-center gap-x-1">
+            
+            <button class="w-7 h-5 flex items-center text-black justify-center bg-[#e2e2e2]">
+              <img class="w-4 h-4" src="./assets/img/chevron-left.svg" alt="Chevron left">
+            </button>
+
+            <button v-for="(num, index) in [1, 2, 3, 4, 5]" :class="index === 0 ? 'bg-orange-500 text-white' : 'bg-white text-black'" class="w-5 h-5 flex items-center text-black justify-center">
+              {{ num }}
+            </button>
+
+            <button class="w-7 h-5 flex items-center text-black justify-center bg-[#e2e2e2]">
+              <img class="w-4 h-4" src="./assets/img/chevron-right.svg" alt="Chevron left">
+            </button>
+
+            <button class="w-7 h-5 flex items-center text-black justify-center bg-[#e2e2e2]">
+              <img class="w-4 h-4" src="./assets/img/chevron-right-double.svg" alt="Chevron left">
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+
+const navigation = [
+    { name: 'Сотрудники', href: '#' },
+    { name: 'Пополнение', href: '#' },
+    { name: 'Настройки', href: '#' },
+    { name: 'Отчёты', href: '#' },
+]
+
+const mobileMenuOpen = ref(false);
+
+</script>
+
+<template>
+    <header class="bg-black">
+        <nav class="max-w-[1470px] mx-auto flex items-center justify-between p-3 lg:px-8">
+            <div class="flex items-center lg:gap-x-16">
+                <div class="flex lg:flex-1">
+                    <a href="#" class="-m-1.5 p-1.5">
+                        <span class="text-white text-[26px]">СУП</span>
+                    </a>
+                </div>
+                <div class="hidden lg:flex lg:gap-x-8">
+                    <a v-for="item in navigation" :key="item.name" :href="item.href" :title="item.name" class="text-base font-medium text-[#E6E6E6] lg:hover:text-white lg:hover:font-bold lg:before:block before:content-[attr(title)] before:font-bold before:h-0 before:overflow-hidden before:invisible">{{ item.name }}</a>
+                </div>
+            </div>
+            <div class="flex lg:hidden">
+                <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+                    <Bars3Icon class="h-8 w-8 text-white" aria-hidden="true" />
+                </button>
+            </div>
+            <div class="hidden lg:flex lg:items-center lg:gap-x-6">
+                <span class="text-base font-medium text-white">1 209 654 руб.</span>
+                <a href="#">
+                    <img src="../assets/img/user.png" alt="User image">
+                </a>
+            </div>
+        </nav>
+        <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+            <div class="fixed inset-0 z-10" />
+            <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div class="flex items-center justify-between">
+                    <a href="#" class="-m-1.5 p-1.5">
+                        <span class="text-black font-bold text-[26px]">СУП</span>
+                    </a>
+                    <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+                        <XMarkIcon class="h-8 w-8 text-black" aria-hidden="true" />
+                    </button>
+                </div>
+                <div class="mt-6">
+                    <div class="-my-6 divide-y divide-gray-500/10">
+                        <div class="space-y-2 py-6">
+                            <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-black">{{ item.name }}</a>
+                        </div>
+                        <div class="pt-4 flex items-center gap-x-6">
+                            <a href="#">
+                                <img class="h-10 w-10 rounded-full" src="../assets/img/user.png" alt="User image">
+                            </a>
+                            <span class="text-lg font-medium text-black">1 209 654 руб.</span>
+                        </div>
+                    </div>
+                </div>
+            </DialogPanel>
+        </Dialog>
+    </header>
+</template>
